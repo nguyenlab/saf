9230c82f929770f33dd8e8d550d8dca69e1e26ce
Danilo S. Carvalho <danilo@jaist.ac.jp>
2017-09-07 11:56:04 +0900

    - Added module init file. - Data model classes are now auto-imported by SAF.

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__author__ = 'danilo@jaist.ac.jp'
+
+from data_model.annotable import Annotable
+from data_model.document import Document
+from data_model.sentence import Sentence
+from data_model.term import Term
+from data_model.token import Token
